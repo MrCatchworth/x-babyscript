bfee4ac2d0753f0dcaf558b088bb5c978859c2ec
MegaJohnny <megajohnny@hotmail.co.uk>
2019-04-14 13:44:38 +0100

    Stop exposing EscapeCode

--- EscapeCode.cs
+++ EscapeCode.cs
@@ -6,7 +6,7 @@ using System.Text.RegularExpressions;
 
 namespace XBabyScript
 {
-    public class EscapeCode
+    internal class EscapeCode
     {
         private static readonly Regex EscapeCodeRegex = new Regex(@"\\(.)");
 
